ad8e7b4325d47b78af7b0c64268782bca264b42e
lrj <lirenjun@camera360.com>
2019-10-22 15:15:23 +0800

    update

--- AnimationDemo/Source/MultiLanguageTexInputAlert.swift
+++ AnimationDemo/Source/MultiLanguageTexInputAlert.swift
@@ -11,7 +11,6 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 import RxSwiftExt
-import IQKeyboardManagerSwift
 
 class MultiLanguageTexInputAlert: UIViewController {
     @IBOutlet weak var textView: UITextView!
@@ -21,7 +20,10 @@ class MultiLanguageTexInputAlert: UIViewController {
     @IBOutlet weak var closeBtn: UIButton!
     let bag = DisposeBag()
     @IBOutlet weak var inputViewCenterY: NSLayoutConstraint!
+    @IBOutlet weak var coverBtn: UIButton!
     fileprivate let selectedModel: BehaviorRelay<TextInputModel?> = .init(value: nil)
+    
+    @IBOutlet weak var containerView: UIView!
     fileprivate lazy var datas: BehaviorRelay<[TextInputModel]> = {
         return BehaviorRelay<[TextInputModel]>(value:
             [ TextInputModel(keyboardType: 0, keyboardDesc: "简体中文", inputContent: nil, textPlaceholder: "字数不超过200字", textInputLength: 2000, isSelect: true),
@@ -48,18 +50,18 @@ class MultiLanguageTexInputAlert: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        IQKeyboardManager.shared.enable = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        IQKeyboardManager.shared.enable = false
     }
     
 }
 
 extension MultiLanguageTexInputAlert {
     fileprivate func congfigUI() {
+        textView.returnKeyType = .done
+        textView.delegate = self
         view.backgroundColor = .clear
         collectionView.delegate = self
         collectionView.registerNibWithCell(PadTypeCollectionViewCell.self)
@@ -80,6 +82,7 @@ extension MultiLanguageTexInputAlert {
             .bind(to: textPlaceholder.rx.isHidden)
             .disposed(by: bag)
         
+        let originInputCenterYCons = self.inputViewCenterY.constant
         let allSavedModels: BehaviorRelay<[TextInputModel]> = .init(value: [])
         let selectedModel = self.selectedModel
         func selected(_ indexPath: IndexPath) {
@@ -176,6 +179,48 @@ extension MultiLanguageTexInputAlert {
             }
             .subscribe()
             .disposed(by: bag)
+        
+        let inputViewCenterY = self.inputViewCenterY
+        Observable.merge(NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification),
+                         NotificationCenter.default.rx.notification(UIResponder.keyboardWillChangeFrameNotification))
+            .map { $0.userInfo }
+            .unwrap()
+                .subscribe(onNext: {[weak self] (info) in
+                    guard let weakSelf = self else {
+                        return
+                    }
+                    if let krect = info[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                       let offset = weakSelf.inputViewOffet(with: krect)
+                        inputViewCenterY?.constant -= offset
+                    }
+                    UIView.animate(withDuration: 0.25) {
+                        weakSelf.view.layoutIfNeeded()
+                    }
+                })
+            .disposed(by: bag)
+        
+        NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+             .map { $0.userInfo }
+             .unwrap()
+                 .subscribe(onNext: {[weak self] (info) in
+                     guard let weakSelf = self else {
+                         return
+                     }
+                     inputViewCenterY?.constant = originInputCenterYCons
+                     UIView.animate(withDuration: 0.25) {
+                         weakSelf.view.layoutIfNeeded()
+                     }
+                 })
+             .disposed(by: bag)
+        
+        coverBtn.rx.tap
+            .subscribe(onNext: {[weak self] (_) in
+                guard let weakSelf = self else {
+                    return
+                }
+                weakSelf.view.endEditing(true)
+            })
+            .disposed(by: bag)
     }
 }
 
@@ -197,6 +242,29 @@ extension MultiLanguageTexInputAlert: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 10
+    }
+}
+
+extension MultiLanguageTexInputAlert {
+    fileprivate func inputViewOffet(with keyboardFrame: CGRect) -> CGFloat {
+        var offset: CGFloat = 0
+        let kY = keyboardFrame.origin.y
+        let convertRect = self.containerView.frame
+        if kY < convertRect.maxY {
+            offset = convertRect.maxY  - kY
+            return offset
+        }
+        return offset
+    }
+}
+
+extension MultiLanguageTexInputAlert: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            view.endEditing(true)
+            return false
+        }
+        return true
     }
 }
 
